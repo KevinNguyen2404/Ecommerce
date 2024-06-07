68876839dd11e4b3e9404c2817b8d8f335fee9d6
KevinNguyen2404 <doanpy20031234@gmail.com>
2024-06-07 21:03:00 +0700

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Carousel from "./components/Carousel_slider/Carousel";
+import Navbar from "./components/Navbar/Navbar";
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Carousel />
+    </div>
+  );
+};
+
+export default App;
